4fd0a88d811a2c14d17a279e586328ff85f6a0cd
Diego2452 <diegovargas087@gmail.com>
2026-07-05 00:56:46 -0600

    fix: remove basePath, deploy to root

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/nuevo', // <--- Agrega esto para que Next.js sepa que está en esa subcarpeta
   trailingSlash: true,
   images: {
     unoptimized: true,
