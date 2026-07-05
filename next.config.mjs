5f401e6a8ebb587e82d30f64dd82f3b254f5e7f8
Diego2452 <diegovargas087@gmail.com>
2026-07-04 19:37:56 -0600

    fix: configurar basePath y htaccess para despliegue en subcarpeta nuevo

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/nuevo', // <--- Agrega esto para que Next.js sepa que está en esa subcarpeta
   trailingSlash: true,
   images: {
     unoptimized: true,
